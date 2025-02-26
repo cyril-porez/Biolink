78b36fa823402b0f99f2a991dd1d2ab9cfdb89c4
cyril <cyril.porez@laplateforme.io>
2025-02-26 14:27:46 +0100

    BIOL-6 feat(database) initialisation postgresql test get_users check

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,28 @@
-use axum::Router;
+use axum::{routing::connect, Router};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 mod routes;
 use crate::routes::create_routes;
+mod database;
+use crate::database::connect_db;
+use crate::database::get_users;
 
 #[tokio::main]
 async fn main() {
+
+    let client = connect_db().await.expect("Impossible de se connecter à PostgreSQL");
+    let users = get_users(&client).await.expect("Erreur lors de la récupération des utilisateurs");
+    println!("Utilisateurs : {:?}", users.get(0));
+    let mut test  = 0;
+    if let Some((id,_,_)) = users.get(0) {
+        test = *id;
+    }
+    println!("Test : {}", test);
+    
+
     let app = create_routes();
+
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Server lancé sur http://{}", listener.local_addr().unwrap());
@@ -16,4 +31,6 @@ async fn main() {
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
+
+    
 }
